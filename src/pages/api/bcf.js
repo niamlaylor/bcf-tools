94bace6128983c1b8c66a68042168bdd7a56862b
Liam Naylor <liam@liamnaylor.com>
2024-07-23 12:18:37 -0600

    add bcf api + useEffect on home page to call it

---
+++ src/pages/api/bcf.js
@@ -0,0 +1,7 @@
+import axios from "axios";
+
+const fetchFerryData = async (req, res) => {
+  console.log("Called bcf api")
+};
+
+export default fetchFerryData;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,14 @@
 import Head from 'next/head';
 import { Inter } from 'next/font/google'
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  useEffect(() => {
+    axios.get('/api/bcf')
+  });
   return (
     <main>
       <Head>
