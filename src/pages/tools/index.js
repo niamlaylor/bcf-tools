91517dfd92d635f6d3030d6abe87a1e3134dead5
Liam Naylor <liam@liamnaylor.com>
2024-07-23 13:28:07 -0600

    add form component + add to index tools page

--- src/pages/tools/index.js
+++ src/pages/tools/index.js
@@ -1,7 +1,14 @@
+import ImportantNotesForm from "../../components/forms/IN-Form";
+import Head from 'next/head';
+
 export default function ToolsLanding() {
   return (
-    <div>
-      Tools
-    </div>
+    <main>
+      <Head>
+        <title>BCF Tools | All Tools</title>
+        <meta name="description" content="Home of all tools BCF to make our lives easier." />
+      </Head>
+      <ImportantNotesForm/>
+    </main>
   )
 }
