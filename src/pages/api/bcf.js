e04c6e893f1fb9a95f059dcfd4c9360300332390
Liam Naylor <liam@liamnaylor.com>
2024-07-23 12:21:38 -0600

    add axios call to bcf api

--- src/pages/api/bcf.js
+++ src/pages/api/bcf.js
@@ -2,6 +2,13 @@ import axios from "axios";
 
 const fetchFerryData = async (req, res) => {
   console.log("Called bcf api")
-};
+  try {
+    const response = await axios.get("https://www.bcferriesapi.ca/v2/capacity/")
+    console.log(response.status)
+  }
+  catch {
+    console.log("API call didn't work :(")
+  }
+}
 
 export default fetchFerryData;
