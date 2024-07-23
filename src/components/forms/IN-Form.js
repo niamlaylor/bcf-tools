b106fb7f771f1bb8e9acdcf1e2b5b61405956f53
Liam Naylor <liam@liamnaylor.com>
2024-07-23 13:48:39 -0600

    add deropdown selector component>

--- src/components/forms/IN-Form.js
+++ src/components/forms/IN-Form.js
@@ -1,21 +1,14 @@
+import DropdownSelector from "./DropdownSelector";
+
 export default function ImportantNotesForm() {
   return (
-    <div class="w-full max-w-xs">
+    <div class="w-full max-w-md">
       <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
         <div class="flex flex-col -mx-3 mb-6">
           <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-state">
             Type of message
           </label>
-          <div class="relative">
-            <select class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
-              <option>Single</option>
-              <option>Double</option>
-              <option>2+ messages</option>
-            </select>
-            <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-              <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
-            </div>
-          </div>
+          <DropdownSelector />
         </div>
       </form>
     </div>
